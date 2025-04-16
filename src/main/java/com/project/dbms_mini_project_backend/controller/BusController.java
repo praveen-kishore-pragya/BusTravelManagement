da884c2cd169ff9d5c58c756bc6c2a54fec7a6a2
praveen-kishore-pragya <praveen.pragya1@gmail.com>
2025-04-16 23:03:22 +0530

    Updated controller and added config

--- src/main/java/com/project/dbms_mini_project_backend/controller/BusController.java
+++ src/main/java/com/project/dbms_mini_project_backend/controller/BusController.java
@@ -1,20 +1,25 @@
 package com.project.dbms_mini_project_backend.controller;
 
 import com.project.dbms_mini_project_backend.entity.Bus;
-import org.springframework.web.bind.annotation.CrossOrigin;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import com.project.dbms_mini_project_backend.service.BusService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
 import com.project.dbms_mini_project_backend.repository.BusRepository;
 
 import java.util.List;
+import java.util.Optional;
 
 @RestController
 @RequestMapping("/api/buses")
 @CrossOrigin(origins = "*")
 public class BusController {
 
+    @Autowired
     private final BusRepository busRepository;
+
+    @Autowired
+    private BusService busService;
 
     public BusController(BusRepository busRepository) {
         this.busRepository = busRepository;
@@ -24,5 +29,31 @@ public class BusController {
     public List<Bus> getAllBuses() {
         return busRepository.findAll();
     }
+
+//    @PutMapping("/api/buses/{id}")
+//    public ResponseEntity<Bus> updateBus(@PathVariable Long id, @RequestBody Bus updatedBus) {
+//        Optional<Bus> busOptional = busRepository.findById(id);
+//        if (busOptional.isPresent()) {
+//            Bus existingBus = busOptional.get();
+//            existingBus.setRoute(updatedBus.getRoute());
+//            existingBus.setDepartureTime(updatedBus.getDepartureTime());
+//            existingBus.setArrivalTime(updatedBus.getArrivalTime());
+//            return ResponseEntity.ok(busRepository.save(existingBus));
+//        } else {
+//            return ResponseEntity.notFound().build();
+//        }
+//    }
+
+    @PutMapping("/api/buses/{id}")
+    public ResponseEntity<Bus> updateBus(@PathVariable Long id, @RequestBody Bus updatedBus) {
+        Bus savedBus = busService.updateBus(id, updatedBus);
+        if(savedBus != null)
+            return ResponseEntity.ok(savedBus);
+        else
+            return ResponseEntity.notFound().build();
+    }
+
+
+
 }
 
